85d0edf9e1c0ee18ec1957907de41429847f409c
rchu6120 <rchu6120@bths.edu>
2019-04-01 08:26:39 -0400

    4/1/19

--- src/MultPractice.java
+++ src/MultPractice.java
@@ -1,5 +1,20 @@
 /**
  * Created by Teacher on 4/1/2019.
  */
-public class MultPractice {
+public class MultPractice implements StudyPractice{
+    private int one;
+    private int two;
+
+    public MultPractice(int num1, int num2){
+        one = num1;
+        two = num2;
+    }
+
+    public String getProblem(){
+        return one + " TIMES " + two;
+    }
+
+    public void nextProblem(){
+        two++;
+    }
 }
